fcbeac882e17bd53ac5c8d94ae1374b949c91989
srodante <srodante@gmail.com>
2021-09-26 20:01:58 -0400

    Debug Data Types

---
+++ src/DebugDataTypes2.java
@@ -0,0 +1,21 @@
+public class DebugDataTypes2 {
+    public static void main(String[] args) {
+    char letter = 'A';
+    short index = 0;
+    boolean answer = true;
+    String message = "I got this";
+
+    System.out.print("Char data type: ");
+    System.out.println(letter);
+
+    System.out.print("Short data type: ");
+    System.out.println(index);
+
+    System.out.print("Boolean data type: ");
+    System.out.println(answer);
+
+    System.out.print("String data type: ");
+    System.out.println(message);
+
+    }
+}
